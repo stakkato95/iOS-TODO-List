63850f489e4764b7f269841d2d74e4df0684d55d
Artsiom Kaliaha <stakkato95@gmail.com>
2015-08-09 16:32:44 +0300

    initial commit from Xcode Source Control

---
+++ TODOList/TODOList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TODOList
+//
+//  Created by Artsiom Kaliaha on 09/08/15.
+//  Copyright (c) 2015 Artsiom Kaliaha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TODOList/TODOList/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TODOList
+//
+//  Created by Artsiom Kaliaha on 09/08/15.
+//  Copyright (c) 2015 Artsiom Kaliaha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
